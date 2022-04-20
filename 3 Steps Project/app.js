2361de8739541ae48aafd1ea951b773128bb6e75
Catalin <catalinungureanu@gmail.com>
2022-04-20 14:46:11 +0300

    fixed burger menu actions

--- 3 Steps Project/app.js
+++ 3 Steps Project/app.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav__menu');
+const menuLinks = document.querySelector('.nav-menu');
 
 menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
