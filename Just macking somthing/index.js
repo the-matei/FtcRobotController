6d11c00fc48578c68118ac0c124bb9f471502261
the-matei <102671097+the-matei@users.noreply.github.com>
2025-09-29 02:09:56 +0300

    Update README.md

---
+++ Just macking somthing/index.js
@@ -0,0 +1,10 @@
+for (var clickButton of document.getElementsByClassName("yes"))
+  clickButton.addEventListener("click", alertMeessage);
+function alertMeessage() {
+  alert("Come and tell me the answer");
+}
+for (var clickButton of document.getElementsByClassName("no"))
+  clickButton.addEventListener("click", alertMeessage);
+function alertMeessage() {
+  alert("Come and tell me the answer");
+}
